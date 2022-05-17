f0f906c92801f210e9e4dbf6153c1d50cb144c99
Namsu Park <nspark@emct.kr>
2022-05-17 14:20:22 +0900

    검증 오나료

--- Scrapers/DreamParkBookingWebView/Form1.Designer.cs
+++ Scrapers/DreamParkBookingWebView/Form1.Designer.cs
@@ -30,31 +30,83 @@ namespace DreamParkBookingWebView
         private void InitializeComponent()
         {
             this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // webView21
             // 
             this.webView21.CreationProperties = null;
             this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
-            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Location = new System.Drawing.Point(3, 33);
             this.webView21.Name = "webView21";
-            this.webView21.Size = new System.Drawing.Size(800, 450);
+            this.webView21.Size = new System.Drawing.Size(1874, 756);
             this.webView21.TabIndex = 0;
             this.webView21.ZoomFactor = 1D;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.webView21, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1880, 812);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 24);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(84, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.webView21);
+            this.ClientSize = new System.Drawing.Size(1880, 812);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -62,6 +114,10 @@ namespace DreamParkBookingWebView
         #endregion
 
         private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Scrapers/DreamParkBookingWebView/Form1.cs
+++ Scrapers/DreamParkBookingWebView/Form1.cs
@@ -21,6 +21,7 @@ namespace DreamParkBookingWebView
         private readonly string LogoutUrl = "https://www.dreamparkcc.or.kr/07membership/logout.asp";
         //Timer Timer = new Timer();
         Random rand = new Random(DateTime.Now.Millisecond);
+        ManualResetEvent mre = new ManualResetEvent(false);
 
         List<PersonInfo> PersonInfos;
         Queue<PersonInfo> BookingQueue = new Queue<PersonInfo>();
@@ -29,18 +30,46 @@ namespace DreamParkBookingWebView
         string IDFormat = "document.getElementById(\"ms_id\").value = \"{0}\"";
         string PasswordFormat = "document.getElementById(\"ms_password\").value = \"{0}\"";
         string SelectDateFormat = "xmlhttpPost(\"/include/reservation/reservation_time.asp?submitDate={0}&bk_div=I&str2=\",\"/include/reservation/reservation_check.asp?submitDate={0}&bk_div=I\");";
-        string SelectTimeFormat = "xmlhttpPost2(\"/include/reservation/reservation_check.asp?submitDate={0}&bk_div=I&bk_time={1}&pin_no=\", \"I\");";
+        string SelectTimeFormat = "javascript:xmlhttpPost2(\"/include/reservation/reservation_check.asp?submitDate={0}&bk_div=I&bk_time={1}&pin_no=\", \"I\");";
         string CheckPhoneFormat = "chkHandPhone(formWait,{0},{1});";
         string CerNumFormat = "document.formWait.serNum.value = '{0}'";
         string GetCertNoFormat = "document.formWait.cert_no.value";
         string ReservationFormat = "sms_send(\"{0}\",\"{1}\",\"I\")";
 
         string[] PhoneFormats = new string[] { "document.getElementById(\"a01_12\").value = \"{0}\"", "document.getElementById(\"a01_12_2\").value = \"{0}\"", "document.getElementById(\"a01_12_3\").value = \"{0}\"" };
+        private bool startBooking;
+
         public Form1()
         {
             InitializeComponent();
             webView21.NavigationCompleted += WebView21_NavigationCompleted;
             GetPersonInfo();
+        }
+
+        private async void WebView21_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (startBooking)
+                return;
+            else
+                startBooking = true;
+
+            if (BookingQueue.TryDequeue(out PersonInfo pinfo))
+            {
+                var result = await Book(pinfo);
+                if (result)
+                {
+                    pinfo.LastReservationDate = DateTime.Now;
+                    SetPersonInfos();
+                }
+
+                webView21.CoreWebView2.Navigate(LogoutUrl);
+                startBooking = false;
+
+                if (BookingQueue.Count > 0)
+                    webView21.CoreWebView2.Navigate(DreamParkurl);
+            }
+            else
+                this.Close();
         }
 
         private void GetPersonInfo()
@@ -53,7 +82,6 @@ namespace DreamParkBookingWebView
             }
         }
 
-
         private void SetPersonInfos()
         {
             var json = JsonConvert.SerializeObject(PersonInfos);
@@ -62,25 +90,41 @@ namespace DreamParkBookingWebView
             File.WriteAllText(PersonInfoFileName, json);
         }
 
-
-        private void Wait(int minSeccond, int maxSeccond)
+        private async Task Wait(int minSeccond, int maxSeccond)
         {
+#if !DEBUG
             int waittime = (int)(minSeccond * 1000 + rand.NextDouble() * (maxSeccond - minSeccond) * 1000);
-            Thread.Sleep(waittime);
+            await Task.Delay(waittime);
+#else
+            await Task.Run(() =>
+            {
+                mre.Reset();
+                mre.WaitOne();
+            });
+#endif
         }
 
-        private string GetTargetDate()
+        private List<DateTime> GetTargetDates()
         {
+            List<DateTime> result = new List<DateTime>();
             var now = DateTime.Now;
             string date;
             if (now.DayOfWeek == DayOfWeek.Monday)
-                date = now.AddDays(14 + rand.Next(0, 4)).ToString("yyyyMMdd");
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    result.Add(now.AddDays(14 + i));
+                }
+            }
             else if (now.DayOfWeek == DayOfWeek.Tuesday)
-                date = now.AddDays(18 + rand.Next(0, 1)).ToString("yyyyMMdd");
-            else
-                date = null;
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    result.Add(now.AddDays(18 + i));
+                }
+            }
 
-            return date;
+            return result;
         }
 
         private async void Form1_Load(object sender, EventArgs e)
@@ -89,54 +133,70 @@ namespace DreamParkBookingWebView
             if (webView21 != null && webView21.CoreWebView2 != null)
             {
                 webView21.CoreWebView2.Settings.AreDefaultScriptDialogsEnabled = false;
-
+                webView21.CoreWebView2.ScriptDialogOpening += CoreWebView2_ScriptDialogOpening;
                 webView21.CoreWebView2.Navigate(DreamParkurl);
             }
         }
 
-        private async void WebView21_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        private void CoreWebView2_ScriptDialogOpening(object sender, CoreWebView2ScriptDialogOpeningEventArgs e)
         {
-            if(BookingQueue.TryDequeue(out PersonInfo pinfo))
-            {
-                await Book(pinfo);
-                pinfo.LastReservationDate = DateTime.Now;
-                webView21.CoreWebView2.Navigate(LogoutUrl);
-                SetPersonInfos();
-
-                if (BookingQueue.Count > 0)
-                    webView21.CoreWebView2.Navigate(DreamParkurl);
-            }
+            if (e.Kind == CoreWebView2ScriptDialogKind.Confirm)
+                e.Accept();
         }
 
-        private async Task Book(PersonInfo item)
+        private async Task<bool> Book(PersonInfo item)
         {
             if (item.LastReservationDate.Date >= DateTime.Now.Date)
-                return;
+                return false;
 
-            var targetDate = GetTargetDate();
-
-            if (string.IsNullOrWhiteSpace(targetDate))
-            {
-                Log.Write("Failed", "예약 가능 일자가 아님");
-                return;
-            }
-
-            Wait(4, 8);
+            await Wait(4, 8);
             var dd = await ExecuteAndLogAsync(string.Format(IDFormat, item.ID));
 
             var ddd = await ExecuteAndLogAsync(string.Format(PasswordFormat, item.Password));
             var result = await ExecuteAndLogAsync("fncLogin()");
 
-            Wait(1, 5);
+            await Wait(1, 5);
             var source = webView21.Source;
             result = await ExecuteAndLogAsync("xmlhttpPost_real('/include/reservation/reservation01.asp','200');");  //숫자는 now 대기인 명수 
-            Wait(1, 5);
+            await Wait(1, 5);
 
-            var selectDateString = string.Format(SelectDateFormat, targetDate);
+
+            //가능 한 시간 가져오기
+            var datesCountstring = await ExecuteAndLogAsync("document.getElementsByClassName(\"bg_red\").length");
+            List<int> availableDates = new List<int>();
+            if (int.TryParse(datesCountstring, out int dateCount))
+            {
+                for (int i = 0; i < dateCount; i++)
+                {
+                    var available = await ExecuteAndLogAsync($"document.getElementsByClassName(\"bg_red\")[{i}].innerHTML");
+
+                    if (!string.IsNullOrWhiteSpace(available))
+                        available = available.Substring(1, available.Length - 2);
+
+                    if (int.TryParse(available, out int day))
+                        availableDates.Add(day);
+                }
+            }
+            else
+            {
+                Log.WriteError("DreamLog", "Failed to Get Date");
+                return false;
+            }
+
+            var dayIndex = rand.Next(0, availableDates.Count - 1);
+            var targetDay = availableDates[dayIndex];
+            var now = DateTime.Now;
+            var year = now.Day > targetDay && now.Month == 12 ? now.Year + 1 : now.Year;
+            var month = now.Day > targetDay ? (now.Month == 12 ? 1 : now.Month + 1) : now.Month;
+            var targetDate = new DateTime(year, month, targetDay);
+            Log.Write("DreamLog", $"target Date {targetDate.ToString("yyyyMMdd")}");
+
+            var targetDateStr = targetDate.ToString("yyyyMMdd");
+            var selectDateString = string.Format(SelectDateFormat, targetDateStr);
             result = await ExecuteAndLogAsync(selectDateString);
 
-            Wait(1, 5);
-            //시간대 선택  8 ~ 11
+            await Wait(1, 5);
+            //시간대 선택  8 ~ 11 ㅅㅣ간대도 있는거만 골라서 선택하도록 수정해야한다.
 
             string targetTime = "";
             if (rand.Next(0, 1) == 1)
@@ -144,28 +204,38 @@ namespace DreamParkBookingWebView
             else
                 targetTime = "08";
 
-            var selectTimeString = string.Format(SelectTimeFormat, targetDate, targetTime);
+            var selectTimeString = string.Format(SelectTimeFormat, targetDateStr, targetTime);
             result = await ExecuteAndLogAsync(selectTimeString);
 
-            Wait(4, 7);
+            await Wait(4, 7);
             result = await ExecuteAndLogAsync(string.Format(PhoneFormats[0], item.Phone[0]));
             result = await ExecuteAndLogAsync(string.Format(PhoneFormats[1], item.Phone[1]));
             result = await ExecuteAndLogAsync(string.Format(PhoneFormats[2], item.Phone[2]));
-
             result = await ExecuteAndLogAsync(string.Format(CheckPhoneFormat, targetTime, string.Join("", item.Phone)));
-            Wait(10, 30);
+            await Wait(10, 30);
 
             result = await ExecuteAndLogAsync(GetCertNoFormat);
+            if (string.IsNullOrWhiteSpace(result))
+            {
+                Log.WriteError("DreamLog", $"Failed to get certnum {result}");
+                return false;
+            }
+            else
+                result = result.Substring(1, result.Length - 2);
+
             var certnumString = string.Format(CerNumFormat, result);
             await ExecuteAndLogAsync(certnumString);
 
 
-            await ExecuteAndLogAsync(string.Format(ReservationFormat, targetDate, targetTime));
-            Wait(2, 4);
+            await ExecuteAndLogAsync(string.Format(ReservationFormat, targetDateStr, targetTime));
+            await Wait(2, 4);
+
+            return true;
         }
 
         private async Task<string> ExecuteAndLogAsync(string command)
         {
+            await Wait(0, 1);
             var result = await webView21.ExecuteScriptAsync(command);
             Log.Write("DreamLog", result);
 
@@ -174,6 +244,27 @@ namespace DreamParkBookingWebView
 
         private async void Form1_SizeChanged(object sender, EventArgs e)
         {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mre.Set();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (BookingQueue.TryDequeue(out PersonInfo pinfo))
+            {
+                await Book(pinfo);
+                pinfo.LastReservationDate = DateTime.Now;
+                webView21.NavigationCompleted -= WebView21_NavigationCompleted;
+                webView21.CoreWebView2.Navigate(LogoutUrl);
+                SetPersonInfos();
+
+                webView21.NavigationCompleted += WebView21_NavigationCompleted;
+                if (BookingQueue.Count > 0)
+                    webView21.CoreWebView2.Navigate(DreamParkurl);
+            }
         }
     }
 
